ee519c3331a82c4408755be94a15cdfb60b366d8
Lucas Williams <lucasnwilliams2008@gmail.com>
2026-01-04 19:33:27 -0500

    Tests query param with AlbumView
    Logs the album ID from the route parameters when the
    AlbumView component is mounted. This is likely for debugging or
    initial data fetching based on the album ID.

--- src/components/Collections/AlbumView.vue
+++ src/components/Collections/AlbumView.vue
@@ -3,7 +3,14 @@
 </template>
 
 <script setup lang="ts">
+import {onMounted} from "vue";
+import {useRoute} from "vue-router";
 
+const route = useRoute()
+
+onMounted(() => {
+  console.log(route.params.albumId)
+})
 </script>
 
 <style scoped>
